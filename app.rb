aad06263b5a4e96a0d9c5c7b4be99f489fcde44a
Ryoma ABE <arayahomuhomu@gmail.com>
2015-07-27 03:18:22 +0900

    🍶リクエストされた月に合わせて日付と曜日は自動で埋めるようにした．よってslimファイルはひとつで十分．

--- app.rb
+++ app.rb
@@ -8,8 +8,27 @@ get '/' do
   slim:index
 end
 
-get '/June' do
-  @objects=CSV.read('csv/June.csv')
-  slim:June
-  #binding.pry
+
+get '/:month' do
+  @requested_month = params[:month].to_i
+  @days = %w(土 日 月 火 水 木 金)
+  set_first_day(@requested_month)
+  @finalday = set_final_day(@requested_month)
+  @objects=CSV.read("csv/#{@requested_month}.csv")
+  slim:output
 end
+
+def set_first_day(month)
+  year = 2015
+  date = 1
+  # Zeller's congruence
+  @first_day_num = (date+(26*(month+1)/10)+year%100+(year%100/4)+5*(year/100)+(year/100/4))%7
+end 
+
+def set_final_day(month)
+  if month <= 7
+    return month%2 == 0 ? 30 : 31
+  else
+    return month%2 == 0 ? 31 : 30
+  end 
+end 
